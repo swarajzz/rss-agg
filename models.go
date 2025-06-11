29d75140a41e7d7dc897511a912e38060d24c8c6
swarajzz <sharma.swaraj0403@gmail.com>
2025-06-11 16:51:52 +0530

    add get post for user functionality

--- models.go
+++ models.go
@@ -78,3 +78,40 @@ func databaseFeedFollowsToFeedFollows(dbFeedFollow []database.FeedFollow) []Feed
 	}
 	return feeds
 }
+
+type Post struct {
+	ID          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string    `json:"title"`
+	Url         string    `json:"url"`
+	Description *string   `json:"description"`
+	PublishedAt time.Time `json:"published_at"`
+	FeedID      uuid.UUID `json:"feed_id"`
+}
+
+func databasePostToPost(dbPost database.Post) Post {
+	var description *string
+	if dbPost.Description.Valid {
+		description = &dbPost.Description.String
+	}
+
+	return Post{
+		ID:          dbPost.ID,
+		CreatedAt:   dbPost.CreatedAt,
+		UpdatedAt:   dbPost.UpdatedAt,
+		Title:       dbPost.Title,
+		Url:         dbPost.Url,
+		Description: description,
+		PublishedAt: dbPost.PublishedAt,
+		FeedID:      dbPost.FeedID,
+	}
+}
+
+func databasePostsToPosts(dbPosts []database.Post) []Post {
+	posts := []Post{}
+	for _, dbFeedFollow := range dbPosts {
+		posts = append(posts, databasePostToPost(dbFeedFollow))
+	}
+	return posts
+}

fcf7ca24b7a8fe67951bee78f83176bf2e89eca8
swarajzz <sharma.swaraj0403@gmail.com>
2025-06-11 12:46:03 +0530

    add aggregation worker

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -19,16 +21,22 @@ type apiConfig struct {
 }
 
 func main() {
+	feed, err := urlToFeed("https://wagslane.dev/index.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(feed)
+
 	godotenv.Load(".env")
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("PORT is ot found in the environment")
+		log.Fatal("PORT is not found in the environment")
 	}
 
 	dbURL := os.Getenv("DB_URL")
 	if dbURL == "" {
-		log.Fatal("DB_URL is ot found in the environment")
+		log.Fatal("DB_URL is not found in the environment")
 	}
 
 	conn, err := sql.Open("postgres", dbURL)
@@ -36,9 +44,12 @@ func main() {
 		log.Fatal("Can't connect to database", err)
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 

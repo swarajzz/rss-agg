71f585ea88c3aaba2d296da3fe35aa964ed1f7bf
swarajzz <sharma.swaraj0403@gmail.com>
2025-06-11 16:26:45 +0530

    create post from aggregation worker

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -21,12 +20,6 @@ type apiConfig struct {
 }
 
 func main() {
-	feed, err := urlToFeed("https://wagslane.dev/index.xml")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(feed)
-
 	godotenv.Load(".env")
 
 	port := os.Getenv("PORT")
